77732d116d570f0667e4e8a69415b3a6cd42deba
Austin K Lanphear <austin@soltek.games>
2025-05-28 16:37:10 -0400

    Update javascript scripts to update html pages more dynamically. Add slideshow to reclamation page.

---
+++ scripts/common.js
@@ -0,0 +1,49 @@
+
+function getSidebar(){
+
+    const title = document.title.toLowerCase();
+    if (!title.startsWith('blog')) {
+        return;
+    }
+
+    const bar = document.getElementById('sidebar');
+    const sidebar = document.createElement('object');
+    sidebar.style.height = '100%';
+    sidebar.type = 'text/html';
+    sidebar.data = '../../common/sidebar.html';
+
+    bar.appendChild(sidebar);
+}
+
+function getHeader(filename = 'common/header.html') {
+
+    const bar = document.getElementById('header');
+    const header = document.createElement('object');
+    header.style.height = '100%';
+    header.style.width = '100%';
+    header.type = 'text/html';
+
+    // Determine path to header file
+    const path = window.location.pathname;
+
+    // Count how deep the file is compared to top level
+    const depth = path.split('/').length - 6;
+
+    // Build the relative path
+    let relativePath = '';
+    for (let i = 0; i < depth; i++){
+        relativePath += '../';
+    }
+
+    relativePath += filename;
+    console.log(relativePath);
+
+    header.data = relativePath;
+    bar.appendChild(header);
+}
+
+
+window.onload = function() {
+    getSidebar();
+    getHeader();
+};
