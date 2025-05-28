a5729863c4ab1035c7f4d2f363b28c712440fa57
Austin K Lanphear <austin@soltek.games>
2025-05-28 16:43:05 -0400

    Update relative path for header

--- scripts/common.js
+++ scripts/common.js
@@ -27,6 +27,12 @@ function getHeader(filename = 'common/header.html') {
     const path = window.location.pathname;
 
     // Count how deep the file is compared to top level
+    const negate = 0;
+    if (path.contains('github.io')){
+        negate = 2;
+    } else {
+        negate = 6;
+    }
     const depth = path.split('/').length - 6;
 
     // Build the relative path
